077715f8f366900987c40e77f9f84835bc66578c
Pascal EDMOND <pascal.edmond@bnpparibas.com>
2025-03-21 14:10:48 +0100

    Initial Commit

---
+++ POCAppIntents/POCAppIntentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  POCAppIntentsApp.swift
+//  POCAppIntents
+//
+//  Created by f19876 on 21/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct POCAppIntentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
